d3bc4c0a4fc2ed9ce32db45249c10396bb56e8d5
contributoropensource <115569777+contributoropensource@users.noreply.github.com>
2023-10-07 08:02:35 +0530

    Create Fibonacci.c

---
+++ Programs/Fibonacci Series/Fibonacci.c
@@ -0,0 +1,16 @@
+#include<stdio>
+int main()
+{
+  int n1=0,n2=1,t,a,i;
+  printf("Enter no of terms:");
+  scanf("%d",&a);
+
+  printf("Fibonacci sequence:");
+  for(i=1;i<=a;i++)
+  {
+    printf("%d\t",n1);
+    t=n1+n2;
+    n1=n2;
+    n2=t;
+}
+}
